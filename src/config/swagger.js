a813bc61ac5bbeb5fa4158e3de61621a96ce6667
Adib23704 <adib23704@gmail.com>
2025-04-12 01:31:01 +0600

    Fix backend URL configuration in Swagger setup to use default localhost

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,7 +13,7 @@ const options = {
 		},
 		servers: [
 			{
-				url: `${process.env.BACKEND_URL}:${process.env.PORT || 3000}`
+				url: `${process.env.BACKEND_URL || 'http://localhost:3000'}`
 			}
 		],
 		components: {
