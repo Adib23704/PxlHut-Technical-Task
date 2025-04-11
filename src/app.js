be94c86f8465b0df8c624176b503c54c04f1fb53
Adib23704 <adib23704@gmail.com>
2025-04-11 18:22:52 +0600

    Refactor project structure: move app.js to src directory and update package.json main entry

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ app.get('/', (req, res) => {
 	res.status(200).json({ message: 'API is running' })
 })
 
-// Error handling middleware
 app.use((err, req, res, _next) => {
 	console.error(err.stack)
 	res.status(500).json({ message: 'Something broke!' })
