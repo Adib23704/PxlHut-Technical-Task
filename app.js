6faa3429b69cd1767e7f5daf5b30b576f6a832ce
Adib23704 <adib23704@gmail.com>
2025-04-11 18:19:55 +0600

    Add security and rate limiting middleware to Express server

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
 const app = express()
 
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100
+})
+app.use(limiter)
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'API is running' })
