7c6432a3d8dbbc92abee48fabe1d54ecaba8f65e
Adib23704 <adib23704@gmail.com>
2025-04-11 20:07:37 +0600

    Enhance password validation rules and add example environment configuration

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -16,7 +16,17 @@ const validateRequest = validations => {
 const registerValidation = [
 	body('name').notEmpty().withMessage('Name is required'),
 	body('email').isEmail().withMessage('Please include a valid email'),
-	body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+	body('password')
+		.isLength({ min: 6 })
+		.withMessage('Password must be at least 6 characters')
+		.matches(/[A-Z]/)
+		.withMessage('Password must contain at least one uppercase letter')
+		.matches(/[a-z]/)
+		.withMessage('Password must contain at least one lowercase letter')
+		.matches(/[0-9]/)
+		.withMessage('Password must contain at least one number')
+		.matches(/[@$!%*?&#]/)
+		.withMessage('Password must contain at least one special character')
 ]
 
 const loginValidation = [
